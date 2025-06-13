7e43e908aaac5c34602a2ed8c36b7848c77eeb65
Rafael batista <rafael.batista.lopes.rb@gmail.com>
2025-06-13 16:32:34 -0300

    icons2

--- src/components/LoginScreen.jsx
+++ src/components/LoginScreen.jsx
@@ -7,7 +7,7 @@ export default function LoginScreen() {
   return (
     <section>
       <div className="flex flex-col justify-center items-center w-full gap-4 pt-15 pb-20 bg-gradient-to-b from-[#B5B6F2] to-[#EFEFFF] md:flex-row">
-        <div className="bg-white rounded-md shadow-md p-6 w-[80%] md:w-3/5 xl:w-2/4">
+        <div className="bg-white rounded-md shadow-md p-6 w-[80%] md:ml-[2rem] md:w-3/5 xl:w-2/4">
           <LoginTopForm />
 
           <LoginForm />
